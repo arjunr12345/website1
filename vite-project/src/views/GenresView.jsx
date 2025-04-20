da2cf99192612ded171a487643bc96a41dd1f6aa
arjun <arjunrajane@hotmail.ca>
2025-04-19 20:36:01 -0400

    edited file name

--- vite-project/src/views/GenresView.jsx
+++ vite-project/src/views/GenresView.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios"
 import { useParams } from "react-router-dom";
-import "./Genres.css"
+import "./GenresView.css"
 
 const GenreView = () => {
     const [movieData, setMovieData] = useState([]);
